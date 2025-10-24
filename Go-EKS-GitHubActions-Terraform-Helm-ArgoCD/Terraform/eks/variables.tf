f00850d2d7d2086f7ab72cebfbf047d61d8bb295
pooja175 <pooja175shah@gmail.com>
2025-10-24 21:57:24 +0100

    Update variables.tf

--- Go-EKS-GitHubActions-Terraform-Helm-ArgoCD/Terraform/eks/variables.tf
+++ Go-EKS-GitHubActions-Terraform-Helm-ArgoCD/Terraform/eks/variables.tf
@@ -35,7 +35,11 @@ variable "endpoint-public-access" {}
 variable "ondemand_instance_types" {
   default = ["t3a.medium"]
 }
-
+variable "image_tag" {
+  type        = string
+  description = "Docker image tag for the Go application"
+  default     = "latest"
+}
 variable "spot_instance_types" {}
 variable "desired_capacity_on_demand" {}
 variable "min_capacity_on_demand" {}
